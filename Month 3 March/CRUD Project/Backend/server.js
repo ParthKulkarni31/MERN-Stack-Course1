fd8834b569ac86e1fbc36e8c31b7995c8d730fb3
ParthKulkarni31 <parthskulkarni311005@gmail.com>
2026-03-07 14:00:23 +0530

    API started in postman

--- Month 3 March/CRUD Project/Backend/server.js
+++ Month 3 March/CRUD Project/Backend/server.js
@@ -1,26 +1,14 @@
 // // Backend Project
 
 // //Node js
-// //Express js
+// //Express js                                                                    
 // //DB MongoDB
 
 // // API list
-
 // //  1. Create iteam - Get data value from frontend {iteam details} and store into DB
-
 // // 2. Update iteam - get iteam details from frontend which iteam we need to update
-
 // // 3. Delete iteam - get iteam details from frontend and on delete this record from database
-
 // // 4. get ALL Records - get ALL records from DB and show to UI frontend
-
-// const getData = () => {
-
-// }
-
-// function getData(){
-    
-// }
 
 console.log("Hello Node Js project is started")
 
@@ -32,13 +20,78 @@ const mongoose = require('mongoose')
 
 app.use(express.json())                   //converts all data in json format
 
-mongoose.connect("mongodb://localhost:27017/item-database").then( () => console.log("MongoDB connected")).catch( (error) => console.log(error) )          //if it get connected then it goes in then and if not it goes in catch
+mongoose.connect("mongodb://localhost:27017/item-database").then(() => console.log("MongoDB connected")).catch((error) => console.log(error))          //if it get connected then it goes in then and if not it goes in catch
 
 
 const itemsSchema = new mongoose.Schema({            // Schema= database table structure model
     name: String,                                    // structure of values stored database
-    description : String,
-    sellingPrice : Number,
+    description: String,
+    sellingPrice: Number,
 })
 
 const Items = new mongoose.model("Items", itemsSchema)
+
+
+//API 1- Create Item
+app.post( "/api/create-item" , async (req , res) =>{
+       try{
+
+        const { name , description , sellingPrice } = req.body
+
+        const saveItem = new Items(
+           { name,
+            description,
+            sellingPrice
+            }
+        )
+          await saveItem.save()
+
+          res.status(201).json( {message : "Item Created" , data : saveItem})
+       } catch (error){
+        console.log(error)
+       }
+} )
+
+
+
+//API 2- Update/Edit
+app.put( "/api/update-item", (req , res) =>{
+  try{
+          
+       } catch (error){
+        console.log(error)
+       }
+} )
+
+
+//API 1- Delete Item
+app.delete("/api/delete-item " , (req , res) =>{
+    try{
+          
+       } catch (error){
+        console.log(error)
+       }
+})
+
+
+//API 1- GetAll Item
+app.get("/api/get-all-item" , async (req , res) => {
+    try{
+        const items = await Items.find()
+
+        res.status(200).json({ message : "Get All Item List" , data : items})
+          
+       } catch (error){
+        console.log(error)
+       }
+})
+
+//Health API
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "Server Is Running" })
+})
+//server Start
+const PORT = 9090
+app.listen(PORT, () => {
+    console.log('Server Started')
+})
